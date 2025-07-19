d264d2cb36ab2531c2c3f33b1937f0f169240ae5
Oleg <steam1-00@list.ru>
2025-07-19 21:36:10 +0300

    add start stream function for take info about subscribed shares

--- api-connection-service/internal/api/stream.go
+++ api-connection-service/internal/api/stream.go
@@ -1,20 +1,22 @@
 package api
 
 import (
+	"context"
 	"fmt"
 	pb "github.com/Point74/tinkoff-candle-streamer/contracts/gen/doc"
 	"google.golang.org/grpc"
 	"log/slog"
+	"sync"
 )
 
 type Stream struct {
-	client pb.MarketDataServiceClient
+	client pb.MarketDataStreamServiceClient
 	logger *slog.Logger
 	conn   *grpc.ClientConn
 }
 
 func NewStream(conn *grpc.ClientConn, logger *slog.Logger) (*Stream, error) {
-	client := pb.NewMarketDataServiceClient(conn)
+	client := pb.NewMarketDataStreamServiceClient(conn)
 	if client == nil {
 		logger.Error("cannot connect to MarketDataService")
 		return nil, fmt.Errorf("cannot connect to MarketDataService")
@@ -25,4 +27,58 @@ func NewStream(conn *grpc.ClientConn, logger *slog.Logger) (*Stream, error) {
 		logger: logger,
 		conn:   conn,
 	}, nil
+}
+
+func (s *Stream) StartStream(instrumentID string) error {
+	s.logger.Info("starting stream", "instrumentID", instrumentID)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	request := &pb.MarketDataRequest{
+		Payload: &pb.MarketDataRequest_SubscribeCandlesRequest{
+			SubscribeCandlesRequest: &pb.SubscribeCandlesRequest{
+				SubscriptionAction: pb.SubscriptionAction_SUBSCRIPTION_ACTION_SUBSCRIBE,
+				Instruments: []*pb.CandleInstrument{
+					{
+						InstrumentId: instrumentID,
+						Interval:     pb.SubscriptionInterval_SUBSCRIPTION_INTERVAL_ONE_MINUTE,
+					},
+				},
+			},
+		},
+	}
+
+	stream, err := s.client.MarketDataStream(ctx)
+	if err != nil {
+		s.logger.Error("cannot connect to MarketDataService", "error", err)
+		return err
+	}
+
+	if err := stream.Send(request); err != nil {
+		s.logger.Error("cannot send request to MarketDataService", "error", err)
+		return err
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		for {
+			resp, err := stream.Recv()
+			if err != nil {
+				s.logger.Error("Error receiving stream data", "error", err)
+				cancel()
+				return
+			}
+
+			s.logger.Info("received candle data", "data", resp)
+		}
+	}()
+
+	wg.Wait()
+
+	return nil
 }
