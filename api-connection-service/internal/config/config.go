7afb637c89d5360297af3357e82a030127d572a7
Oleg <steam1-00@list.ru>
2025-07-22 21:35:24 +0300

    add config for kafka

--- api-connection-service/internal/config/config.go
+++ api-connection-service/internal/config/config.go
@@ -1,15 +1,19 @@
 package config
 
 type Config struct {
-	APIHost  string
-	APIToken string
-	TLS      string
+	APIHost     string
+	APIToken    string
+	TLS         string
+	KafkaBroker string
+	KafkaTopic  string
 }
 
 func LoadConfig() (*Config, error) {
 	return &Config{
-		APIHost:  "invest-public-api.tinkoff.ru:443",
-		APIToken: "your-api-token",
-		TLS:      "certificate/tinkoff.pem",
+		APIHost:     "invest-public-api.tinkoff.ru:443",
+		APIToken:    "your-api-token",
+		TLS:         "certificate/tinkoff.pem",
+		KafkaBroker: "localhost:9092",
+		KafkaTopic:  "candle_data",
 	}, nil
 }
