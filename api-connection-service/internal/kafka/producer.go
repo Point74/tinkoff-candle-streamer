02cfba3300b8819e11c868dfd10bf29c94c8c6aa
Oleg <steam1-00@list.ru>
2025-07-22 22:01:00 +0300

    add check connection to kafka; parameter topic

--- api-connection-service/internal/kafka/producer.go
+++ api-connection-service/internal/kafka/producer.go
@@ -1,31 +1,42 @@
 package kafka
 
 import (
-	"api-connection-service/internal/config"
 	"context"
 	"fmt"
 	"github.com/twmb/franz-go/pkg/kgo"
 	"log/slog"
+	"time"
 )
 
 type Producer struct {
 	client *kgo.Client
 	logger *slog.Logger
+	topic  string
 }
 
-func NewProducer(cfg *config.Config, brokers []string, logger *slog.Logger) (*Producer, error) {
+func NewProducer(brokers string, logger *slog.Logger) (*Producer, error) {
 	if len(brokers) == 0 {
 		return nil, fmt.Errorf("no brokers provided")
 	}
 
 	opts := []kgo.Opt{
-		kgo.SeedBrokers(brokers...),
+		kgo.SeedBrokers(brokers),
 		kgo.RequiredAcks(kgo.AllISRAcks()),
 	}
 
 	client, err := kgo.NewClient(opts...)
 	if err != nil {
 		logger.Error("Error creating Kafka Producer", "error", err, "brokers", brokers)
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.Ping(ctx); err != nil {
+		logger.Error("Error connect to Kafka Producer", "error", err, "brokers", brokers)
+		client.Close()
+
 		return nil, err
 	}
 
