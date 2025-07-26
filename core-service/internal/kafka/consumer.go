871cef9a3c4fbb2d3ab60ec2cdcb210a8c6a04ef
Oleg <steam1-00@list.ru>
2025-07-27 00:24:04 +0300

    add constructor for kafka consumer; close kafka consumer

---
+++ core-service/internal/kafka/consumer.go
@@ -0,0 +1,57 @@
+package kafka
+
+import (
+	"context"
+	"fmt"
+	"github.com/twmb/franz-go/pkg/kgo"
+	"log/slog"
+	"time"
+)
+
+type Consumer struct {
+	client *kgo.Client
+	logger *slog.Logger
+	topic  string
+	group  string
+}
+
+func NewConsumer(brokers string, logger *slog.Logger) (*Consumer, error) {
+	if len(brokers) == 0 {
+		return nil, fmt.Errorf("no brokers provided")
+	}
+
+	opts := []kgo.Opt{
+		kgo.SeedBrokers(brokers),
+		kgo.ConsumeTopics("tinkoff-candle"),
+		kgo.ConsumerGroup("consumer-group"),
+	}
+
+	client, err := kgo.NewClient(opts...)
+	if err != nil {
+		logger.Error("Error creating Kafka Consumer", "error", err, "brokers", brokers)
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.Ping(ctx); err != nil {
+		logger.Error("Error connect to Kafka Consumer", "error", err, "brokers", brokers)
+		client.Close()
+
+		return nil, err
+	}
+
+	return &Consumer{
+		client: client,
+		logger: logger,
+		topic:  "tinkoff-candle",
+		group:  "consumer-group",
+	}, nil
+}
+
+func (p *Consumer) Close() {
+	if p.client != nil {
+		p.client.Close()
+	}
+}
