7cb005298800174833f643278ac92ab78d791502
Oleg <steam1-00@list.ru>
2025-08-04 18:22:25 +0300

    add migration; add sslmode=disable to fix the db connection error; change in database field due to collision

--- core-service/internal/db/postgres/postgres.go
+++ core-service/internal/db/postgres/postgres.go
@@ -2,18 +2,28 @@ package postgres
 
 import (
 	"context"
+	"core-service/internal/db"
+	"errors"
 	"fmt"
 	"github.com/Point74/tinkoff-candle-streamer/config"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5"
 	"log/slog"
 )
 
+const (
+	migrationsPath = "internal/db/migrations"
+)
+
 type Storage struct {
-	db *pgx.Conn
+	database *pgx.Conn
+	logger   *slog.Logger
 }
 
 func New(ctx context.Context, cfg *config.Config, logger *slog.Logger) (*Storage, error) {
-	dbUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+	dbUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		cfg.PostgresUser,
 		cfg.PostgresPassword,
 		cfg.PostgresHost,
@@ -21,26 +31,53 @@ func New(ctx context.Context, cfg *config.Config, logger *slog.Logger) (*Storage
 		cfg.PostgresDB,
 	)
 
-	db, err := pgx.Connect(ctx, dbUrl)
+	database, err := pgx.Connect(ctx, dbUrl)
 	if err != nil {
 		logger.Error("Unable to connect to database", "error", err)
 		return nil, err
 	}
 
-	if err := db.Ping(ctx); err != nil {
+	if err := database.Ping(ctx); err != nil {
 		logger.Error("Ping to connect database failed", "error", err)
-		db.Close(ctx)
+		database.Close(ctx)
+		return nil, err
+	}
+
+	if err := runMigrations(dbUrl); err != nil {
+		logger.Error("Running migrations failed", "error", err)
 		return nil, err
 	}
 
 	logger.Info("Connected to database")
+	logger.Info("Running migrations succeeded", "url", dbUrl)
 
-	return &Storage{db: db}, nil
+	return &Storage{
+		database: database,
+		logger:   logger,
+	}, nil
+}
+
+func runMigrations(dbUrl string) error {
+	m, err := migrate.New("file://"+migrationsPath, dbUrl)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Storage) Save(ctx context.Context, p *db.Page) error {
+
+	return nil
 }
 
 func (s *Storage) Close(ctx context.Context) error {
-	if s.db != nil {
-		return s.db.Close(ctx)
+	if s.database != nil {
+		return s.database.Close(ctx)
 	}
 
 	return nil
